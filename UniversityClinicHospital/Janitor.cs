7d02a11ba832c49a133cdeb44a53604037efa01c
1robnor <robnorspeaks@gmail.com>
2020-01-30 16:31:41 -0500

    created janitor class

---
+++ UniversityClinicHospital/Janitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityClinicHospital
+{
+    public class Janitor : Employee
+    {
+        //properties
+
+
+        //Constructors
+
+
+        //methods
+
+
+    }
+}
