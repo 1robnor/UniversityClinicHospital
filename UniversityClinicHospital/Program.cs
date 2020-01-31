92db97dd8535e0a20b6a9fe053f0dd8a6d370fc1
dealer426 <burnssamuel01@gmail.com>
2020-01-30 21:31:13 -0500

    Built Menu for Main Program

--- UniversityClinicHospital/Program.cs
+++ UniversityClinicHospital/Program.cs
@@ -17,14 +17,46 @@ namespace UniversityClinicHospital
             employeeList.Add(new Receptionist { EmployeeId = 3, EmployeeName = "Rachael"});
             employeeList.Add(new Janitor { EmployeeId = 4, EmployeeName = "Steve"});
 
-            foreach (Employee employee in employeeList)
-            {
-                Console.WriteLine($"{employee.EmployeeId} {employee.EmployeeName}");
-            }
-
+            
             //Create Patient
             Patient myPatient = new Patient(1, "Sam");
             Console.WriteLine($"{myPatient.PatientId} {myPatient.PatientName}");
+
+
+            bool hospitalIsRunning = true;
+
+            while(hospitalIsRunning)
+            {
+
+                Console.WriteLine("1. List all Employees");
+                Console.WriteLine("2. Pay All Employees");
+                Console.WriteLine("3. Exit");
+                string userInput = Console.ReadLine();
+
+
+                switch (userInput)
+                {
+                    case "1":
+                        foreach (Employee employee in employeeList)
+                        {
+                            Console.WriteLine($"{employee.EmployeeId} {employee.EmployeeName}");
+                        }
+                        break;
+                    case "2":
+                        foreach (Employee employee in employeeList)
+                        {
+                            employee.PaySalary();
+                        }
+                        break;
+                    case "3":
+                        hospitalIsRunning = false;
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
 
             Console.ReadKey();
         }
