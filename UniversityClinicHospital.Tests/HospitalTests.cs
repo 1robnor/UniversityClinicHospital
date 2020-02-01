8afd05fb566550102c1959b438b44604161de563
dealer426 <burnssamuel01@gmail.com>
2020-02-01 18:14:44 -0500

    Added first hospital test

---
+++ UniversityClinicHospital.Tests/HospitalTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UniversityClinicHospital.Tests
+{
+    public class HospitalTests
+    {
+        [Fact]
+        public void Class_Can_Creat_Hospital_Class()
+        {
+            Hospital myHosptial = new Hospital();
+        }
+    }
+}
